ccb0df82497199b03d87863004099d95cf14f311
Siddharth Krishnan <sidkrishnan@protonmail.com>
2026-09-01 00:08:32 +0530

    json_hello

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
 #[macro_use] extern crate rocket;
+use serde_json::{Value, json};
 
 #[get("/")]
 fn hello() -> &'static str {
-    "Hello, world!"
+    "Hello, world!\n"
 }
+
+#[get("/json")]
+fn json_hello() -> Value {
+    json!({"message": "Hello, world!\n"})
+}
+
 
 #[rocket::main]
 async fn main() {
 
     let _ = rocket::build()
-            .mount("/", routes![hello])
+            .mount("/", routes![hello, json_hello])
             .launch()
             .await;
 
