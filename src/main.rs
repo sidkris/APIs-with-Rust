bfe1892af93dea0bf1db96fd0b71195bbd757943
Siddharth Krishnan <sidkrishnan@protonmail.com>
2026-09-01 08:55:00 +0530

    C:/Program Files/Git/people

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,38 @@ fn hello() -> &'static str {
 
 #[get("/json")]
 fn json_hello() -> Value {
-    json!({"message": "Hello, world!\n"})
+    json!({"message": "Hello, world!"})
 }
+
+#[get("/people")]
+fn view_people_data() -> Value {
+    json!([{"id" : 1, "name": "Sid Krishnan", "email" : "sid@sidkrishnan.com"}, {"id" : 2, "name": "Siddharth Krishnan", "email" : "not available"}])
+}
+
+#[get("/people/<id>")]
+fn view_people_data_by_id(id: i32) -> Value {
+    let people = view_people_data();
+
+    let people_array = people.as_array().unwrap();
+
+    for person in people_array {
+        if person["id"] == id {
+            return person.clone();
+        }
+    }
+
+    json!({
+        "error": "Person not found"
+    })
+}
+
 
 
 #[rocket::main]
 async fn main() {
 
     let _ = rocket::build()
-            .mount("/", routes![hello, json_hello])
+            .mount("/", routes![hello, json_hello, view_people_data, view_people_data_by_id])
             .launch()
             .await;
 
